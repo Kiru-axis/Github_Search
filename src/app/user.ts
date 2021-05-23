7b244a766066f474d8af3a95c852fa0f5b731fb8
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-23 21:57:49 +0300

    populate the user class with data

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,11 @@
 export class User {
-}
+    constructor(public login:string, 
+        public avatar_url:string,
+        public bio:string,
+        public public_repos:number,
+        public created_at:Date
+        ){
+
+    }
+
+};
