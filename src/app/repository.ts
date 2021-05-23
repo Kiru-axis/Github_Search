04345bcba0c4070fb6690f0a5b56cc8ff1a1e94d
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-23 21:59:25 +0300

    populate the repository class with userData

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,11 @@
 export class Repository {
+    constructor(
+        public name:string,
+        public descriptin:string,
+        public updated_at:Date,
+        public clone_url:string,
+        public language:string
+    ){
+
+    }
 }
