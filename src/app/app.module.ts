af5f11ba94233086523138162394e78153d9cd08
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-05-23 21:53:42 +0300

    generating gitForm and gitResult components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GitFormComponent } from './git-form/git-form.component';
+import { GitResultComponent } from './git-result/git-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitFormComponent,
+    GitResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
